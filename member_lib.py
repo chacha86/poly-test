c478074c3368fd424fbfb0491fca18a1ecc20dba
chacha86 <taejincha0619@gmail.com>
2022-04-25 14:17:21 +0900

    Update member_lib.py

--- member_lib.py
+++ member_lib.py
@@ -9,7 +9,7 @@ def signup() :
   print("회원가입이 완료되었습니다.")
   
 def login() :
-    global logined_member
+    logined_member
     
     login_id = input("아이디 : ")
     login_pw = input("비밀번호 : ") 
@@ -19,16 +19,15 @@ def login() :
     if member == None :
         print("잘못된 회원정보입니다.")
         return
-
-    # 빈칸 채우기4
-    _______________ = member
+    
+    ____________________
     print("{}님! 반갑습니다!!".format(member["nickname"]))
         
     
 def get_member_by_id_and_pw(login_id, login_pw) :
-    member = __________________(login_id)  # 빈칸 채우기5
+    member = get_member_by_id(login_id)
     
-    if member == None :
+    if member != None :
         return None
     
     if member["login_pw"] == login_pw :
@@ -36,6 +35,3 @@ def get_member_by_id_and_pw(login_id, login_pw) :
         
             
 def get_member_by_id(login_id) :
-    for member in members :
-        if member["login_id"] == login_id :
-            return member
